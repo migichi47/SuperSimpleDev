bb0281de48862c466d3a77433682f5a14ebfffb3
Peter Migichi <petermigichi47@gmail.com>
2026-06-24 17:14:43 +0300

    Delet products fro cart when clicking delete

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,6 +1,6 @@
 import { products } from "../data/products.js";
 
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 1,
 }, {
@@ -25,4 +25,16 @@ export function addToCart(productId) {
     quantity: 1
   });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  })
+
+  cart = newCart;
 }
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import { removeFromCart } from '../data/cart.js';
 
 let cartSummaryHTML = '';
 
@@ -31,10 +32,10 @@ cart.forEach((cartItem) => {
           <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
           <div class="product-quantity">
             <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary" >
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -85,3 +86,12 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const {productId} = link.dataset;
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  })
